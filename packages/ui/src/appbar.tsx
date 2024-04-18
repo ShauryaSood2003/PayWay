103b6a3a18d92ee4adb8e23ed4500946298a78c7
Shaurya <shauryasood293@gmail.com>
2024-04-18 18:25:58 +0530

    removed hydration error

--- packages/ui/src/appbar.tsx
+++ packages/ui/src/appbar.tsx
@@ -12,7 +12,7 @@ export const AppBar=({user,onSignin,onSignout}:AppBarProps)=>{
     return(
         <div className="flex justify-between border-b px-4">
             <div className="text-lg flex flex-col justify-center">
-                PayTM
+                PayWay
             </div>
             <div className="flex flex-col justify-center pt-2">
                 <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
